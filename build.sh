60d5a63161199cd62b6d89c9e367c0e5c38c28b4
irayspace <irayspacii@gmail.com>
2025-09-11 21:56:44 +0800

    feat: added boto3

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ECR_URL=$1
 
@@ -6,6 +7,8 @@ if [ ! $ECR_URL ]; then
     echo "ECR_URL is not set. Follow this command: ./build.sh <ecr-url>"
     exit 1
 fi
+
+poetry export --without-hashes --format=requirements.txt > requirements.txt
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URL
 
