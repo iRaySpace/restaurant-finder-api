c3390fd982d5debb0e38bc88391e1583a427f511
irayspace <irayspacii@gmail.com>
2025-09-11 09:58:51 +0800

    feat: Dockerfile and requirements.txt export from poetry

--- app/__main__.py
+++ app/__main__.py
@@ -4,9 +4,4 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    uvicorn.run(
-        "app.api:get_api",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-    )
+    uvicorn.run("app.api:get_api", host="0.0.0.0", port=8000)
