c35640b5a0ba74d503996637b6d4c5afe10cc242
irayspace <irayspacii@gmail.com>
2025-09-12 10:05:01 +0800

    fix: logger

--- app/logger.py
+++ app/logger.py
@@ -2,6 +2,7 @@ import os
 import logging
 import datetime
 import watchtower
+from boto3.session import Session
 
 
 _logger = None
@@ -22,7 +23,9 @@ def register_cw_logger():
     _logger = logging.getLogger("ErrorLogger")
 
     now = int(datetime.datetime.now().timestamp())
+    boto3_session = Session(region_name="us-east-1")
     handler = watchtower.CloudWatchLogHandler(
+        boto3_client=boto3_session.client("logs"),
         log_group_name=log_group_name,
         log_stream_name=str(now),
     )
